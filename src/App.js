8d52aa4cc802e3559987dcdec25112ba78c97e6d
DESKTOP-VHLAITO\mert1 <mert172317@gmail.com>
2022-07-05 21:31:50 +0300

    conditional content

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import HelloWorld from './components/HelloWorld';
 function App() {
   const onClick = () => {
     alert("P etiketine Tıklandı")
-    const arr = [... data]
-    arr[0].name="Mahmut"
+    const arr = [...data]
+    arr[0].name = "Mahmut"
     setData(arr);
 
   }
@@ -21,20 +21,27 @@ function App() {
   }
 
 
-  const [data, setData] = useState([{ name: "Yasin" }, { name: "Ali" }, { name: "Veli" }])
+  const [data, setData] = useState([{ name: "Yasin" }, { name: "ahmet" }, { name: "Hikmet" }, { name: "ab" }, { name: "Ali" }, { name: "Veli" }])
   return (
     <div className="App">
       <header className="App-header">
-        {componentList}
 
         <h3>Liste</h3>
-        <ul>
-          {data.map((item, index) => {
-            return <li key={index} onClick={() => {
-              onClickLi(item.name)
-            }}>{item.name}</li>
-          })}
-        </ul>
+        {data.map((item, index) => {
+          if (item.name.includes("Hikmet")) {
+            return null
+          }
+          if (item.name === "ahmet") {
+            return <h2>{ item.name.charAt(0).toUpperCase()}</h2>
+          }
+          if (item.name.length > 3) {
+            return <h1 key={index}>{item.name}</h1>
+          }
+          return <p key={index}
+            onClick={() => { onClickLi(item.name) }}>
+            {item.name.length <= 2 ? "İsminiz Geçersiz" : item.name}
+          </p>
+        })}
       </header>
     </div>
   );
